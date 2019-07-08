6679eb057cf24e4cf935d37ca1ec17cec0a17ca9
maslke <maslke@outlook.com>
2019-07-08 13:28:34 +0800

    zuul的filter

---
+++ chapter6/zuulsvr/src/main/java/com/maslke/spring/demos/zuulsvr/filters/UserContextFilter.java
@@ -0,0 +1,39 @@
+package com.maslke.spring.demos.zuulsvr.filters;
+
+import com.maslke.spring.demos.zuulsvr.utils.UserContext;
+import com.maslke.spring.demos.zuulsvr.utils.UserContextHolder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+public class UserContextFilter implements Filter {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserContextFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+
+        UserContextHolder.getContext().setCorrelationId(httpServletRequest.getHeader(UserContext.CORRELATION_ID));
+        UserContextHolder.getContext().setUserId(httpServletRequest.getHeader(UserContext.USER_ID));
+        UserContextHolder.getContext().setAuthToken(httpServletRequest.getHeader(UserContext.AUTH_TOKEN));
+        UserContextHolder.getContext().setOrgId(httpServletRequest.getHeader(UserContext.ORG_ID));
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
